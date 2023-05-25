b81680f0322fab70fee9586ffa8eb7eb41b20370
Sajid Shanta <Sajid@admin-dk02.btraccl.com>
2023-05-25 15:27:35 +0600

    day 1 part 2 done

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Sajid Shanta on 25/5/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+
+
+}
+
