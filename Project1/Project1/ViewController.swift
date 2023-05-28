eb70ccf37d15592d8854ae7d62937cfe5319f614
Sajid Shanta <Sajid@Mohammads-Mac-mini.local>
2023-05-28 10:59:54 +0600

    day 1 part 3: Designing our interface

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -24,7 +24,21 @@ class ViewController: UITableViewController {
         }
         print(pictures)
     }
-
-
+    
+    // number of row in table
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    // content of each row
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = pictures[indexPath.row]
+        
+        cell.contentConfiguration = content
+        return cell
+    }
 }
 
