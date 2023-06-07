8b2f245e2859950c591d5980a3228969a98e1a5d
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-07 15:43:54 +0600

    project 5: Checking for valid answers

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -68,15 +68,27 @@ class ViewController: UITableViewController {
     }
     
     func isPossible(word: String) -> Bool {
+        guard var temWord = title?.lowercased() else { return false }
+        for letter in word {
+            if let position = temWord.firstIndex(of: letter) {
+                temWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
         return true
     }
     
     func isOriginal(word: String) -> Bool {
-        return true
+        return !usedWords.contains(word)
     }
     
     func isReal(word: String) -> Bool {
-        return true
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count) // since NSRAnge is from Objective C, it dosn't work well with swift string. that's why we use UTF16
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
     }
 }
 
