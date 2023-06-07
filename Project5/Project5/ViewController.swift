0e8737e61517378572e6106bf456c714fa774f28
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-07 14:20:20 +0600

    project 5: Reading from disk: contentsOfFile

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWord = try? String(contentsOf: startWordURL) {
+                allWords = startWord.components(separatedBy: "\n")
+            }
+        }
+        if allWords.isEmpty {
+            allWords = ["odmowdr"]
+        }
+        
+        startGame()
     }
-
-
+    
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
 }
 
