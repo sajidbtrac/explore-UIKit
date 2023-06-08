e536d8a8b572b3470db73803d713b21b72172b9f
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-08 16:23:24 +0600

    project6: Creating the basic UI: UITabBarController

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -7,13 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var petitions: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title here"
+        cell.detailTextLabel?.text = "Subtitle here"
+        return cell
+    }
 }
 
