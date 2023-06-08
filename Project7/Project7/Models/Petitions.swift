994e4dfffdddf3d4ccaf271cc54f9819ae153a47
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-08 16:49:16 +0600

    project7: Parsing JSON using the Codable protocol

---
+++ Project7/Project7/Models/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by BTSL.SAJID on 8/6/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var petitions: [String] = []
+    var petitions: [Petition] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(data: data)
+            }
+        }
+    }
+    
+    func parse(data: Data) {
+        let decoder = JSONDecoder()
+        
+        if let petitionsJSON = try? decoder.decode(Petitions.self, from: data) {
+            petitions = petitionsJSON.results
+            tableView.reloadData()
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -22,8 +37,9 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title here"
-        cell.detailTextLabel?.text = "Subtitle here"
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
         return cell
     }
 }
