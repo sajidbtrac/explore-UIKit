f6892253d01dc5a5a93639c3814d80023be46829
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-08 17:11:12 +0600

    project7: Rendering a petition: loadHTMLString

--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -42,5 +42,11 @@ class ViewController: UITableViewController {
         cell.detailTextLabel?.text = petition.body
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
