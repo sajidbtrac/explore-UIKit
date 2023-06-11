a3939990c33e90b36d2522b81fec902e8352ca17
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-11 16:15:01 +0600

    project 9: Setting up

---
+++ Project9/Project9/Models/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by BTSL.SAJID on 8/6/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
