677a2f548f6d7fd4de185598ef97a3c0d0a40939
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-12 15:05:10 +0600

    UICollectionView data sources

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to deque Person Cell")
+        }
+//        cell.imgView = systemImage
+        cell.name.text = "Shanta"
+        return cell
+    }
 }
-
