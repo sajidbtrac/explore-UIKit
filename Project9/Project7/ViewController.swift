d8bb85be952b6c6215435ab8565e6d8920bd23df
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-12 10:56:41 +0600

    project9: day 39 done

--- Project9/Project7/ViewController.swift
+++ Project9/Project7/ViewController.swift
@@ -10,33 +10,37 @@ import UIKit
 class ViewController: UITableViewController {
     
     var petitions: [Petition] = []
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         
         let urlString: String
-        if navigationController?.tabBarItem.tag == 1 {
-            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
-        } else {
+        if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
         
-//        if let url = URL(string: urlString) {
-//            if let data = try? Data(contentsOf: url) {
-//                parse(json: data)
-//            } else {
-//                showError()
-//            }
-//        } else {
-//            showError()
-//        }
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                parse(data: data)
-                return
+        DispatchQueue.global(qos: .userInteractive).async{ [weak self] in
+            if let url = URL(string: urlString){
+                if let data = try? Data(contentsOf: url){
+                    self?.parse(json: data)
+                    return
+                }
+            }
+//            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+            DispatchQueue.main.async { [weak self] in
+                self?.showError()
             }
         }
-        showError()
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        }
     }
     
     func showError() {
@@ -45,15 +49,6 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     
-    func parse(data: Data) {
-        let decoder = JSONDecoder()
-        
-        if let petitionsJSON = try? decoder.decode(Petitions.self, from: data) {
-            petitions = petitionsJSON.results
-            tableView.reloadData()
-        }
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return petitions.count
     }
