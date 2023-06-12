a173eaac9e2e39c7dd6c00f3c342a4a0b0493656
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-12 16:00:49 +0600

    Project 10: Connecting up the people

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -18,15 +18,26 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
             fatalError("Unable to deque Person Cell")
         }
-//        cell.imgView = systemImage
-        cell.name.text = "Shanta"
+        
+        let person = people[indexPath.item]
+        
+        cell.name.text = person.name
+        
+        let pathURL = getDocumentsDirectory().appending(path: person.image)
+        cell.imgView.image = UIImage(contentsOfFile: pathURL.path())
+        
+        cell.imgView.layer.borderWidth = 2
+        cell.imgView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imgView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
         return cell
     }
     
@@ -57,5 +68,20 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     func getDocumentsDirectory() -> URL {
         let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         return path[0]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.textFields?[0].text = person.name
+        ac.addAction(UIAlertAction(title: "Add", style: .default){ _ in
+            guard let newName = ac.textFields?[0].text else { return }
+            person.name = newName
+            collectionView.reloadItems(at: [indexPath])
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
     }
 }
