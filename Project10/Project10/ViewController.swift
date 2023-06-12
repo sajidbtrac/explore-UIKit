6ddc75b77dc7b822d6381f965a0a8c5d66f42596
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-12 14:23:44 +0600

    project10: Setting up

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by BTSL.SAJID on 12/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
