d08048c7b7c2d1c1ad5be497cb664684fbb32c18
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-13 12:26:59 +0600

    Special effects: SKEmitterNode

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -129,6 +129,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
 
     func destroy(ball: SKNode) {
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
+        
         ball.removeFromParent()
     }
     
