15c48a71db24a714911555e2d6b3d80f1511e899
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-13 12:54:59 +0600

    Project 12: setting up

---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by BTSL.SAJID on 12/6/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
---
+++ Project12b/Project10/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by BTSL.SAJID on 12/6/23.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    var people: [Person] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: .add, style: .plain, target: self, action: #selector(addNewPerson))
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to deque Person Cell")
+        }
+        
+        let person = people[indexPath.item]
+        
+        cell.name.text = person.name
+        
+        let pathURL = getDocumentsDirectory().appending(path: person.image)
+        cell.imgView.image = UIImage(contentsOfFile: pathURL.path())
+        
+        cell.imgView.layer.borderWidth = 2
+        cell.imgView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+        cell.imgView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
+        return cell
+    }
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        if UIImagePickerController.isSourceTypeAvailable(.camera) { // not available in simulator, only on phone
+            picker.sourceType = .camera
+        }
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        collectionView.reloadData()
+        
+        dismiss(animated: true) //dismiss the image picker
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Choose", message: "rename or delete the person", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Rename Person", style: .default) { [weak self] _ in
+            let renameAc = UIAlertController(title: "Rename Person", message: nil, preferredStyle: .alert)
+            renameAc.addTextField()
+            renameAc.textFields?[0].text = person.name
+            renameAc.addAction(UIAlertAction(title: "Add", style: .default){ _ in
+                guard let newName = renameAc.textFields?[0].text else { return }
+                person.name = newName
+                collectionView.reloadItems(at: [indexPath])
+            })
+            renameAc.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            self?.present(renameAc, animated: true)
+        })
+        ac.addAction(UIAlertAction(title: "Delete Person", style: .destructive) { [weak self] _ in
+            self?.people.remove(at: indexPath.item)
+            collectionView.reloadData()
+        })
+        present(ac, animated: true)
+    }
+}
