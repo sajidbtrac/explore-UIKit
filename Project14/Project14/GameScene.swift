d7265f4028a894fd349836e04df1dbceefab3273
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-14 14:45:57 +0600

    Project 14: Getting up and running: SKCropNode

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -8,11 +8,51 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var  slots = [WhackSlot]()
+    
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "whackBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.position = CGPoint(x: 8, y: 8)
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        
+        for i in 0..<5 {
+            createSlot(at: CGPoint(x: 100 + (i * 170), y: 410))
+        }
+        for i in 0..<4 {
+            createSlot(at: CGPoint(x: 185 + (i * 170 ), y: 320))
+        }
+        for i in 0..<5 {
+            createSlot(at: CGPoint(x: 100 + (i * 170), y: 230))
+        }
+        for i in 0..<4 {
+            createSlot(at: CGPoint(x: 185 + (i * 170), y: 130))
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+    }
+    
+    func createSlot(at position: CGPoint) {
+        let slot = WhackSlot()
+        slot.configure(at: position)
+        addChild(slot)
+        slots.append(slot)
     }
 }
---
+++ Project14/Project14/WhackSlot.swift
@@ -0,0 +1,32 @@
+//
+//  WhackSlot.swift
+//  Project14
+//
+//  Created by BTSL.SAJID on 14/6/23.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        charNode = SKSpriteNode(imageNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+}
