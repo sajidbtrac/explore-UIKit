a01c585e4994cd0b17b5364bbea16b0626a623d8
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-14 15:25:08 +0600

    Project 14: Penguin, show thyself: SKAction moveBy(x:y:duration:)

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -17,6 +17,8 @@ class GameScene: SKScene {
         }
     }
     
+    var popupTime = 0.85
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "whackBackground")
         background.position = CGPoint(x: 512, y: 384)
@@ -43,6 +45,10 @@ class GameScene: SKScene {
         for i in 0..<4 {
             createSlot(at: CGPoint(x: 185 + (i * 170), y: 130))
         }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -54,5 +60,25 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy() {
+        popupTime *= 0.991 // decrease upopup time slowly
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) < 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) < 8 { slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) < 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) < 11 { slots[4].show(hideTime: popupTime) }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay..<maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
     }
 }
