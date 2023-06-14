e748423e9e246135090804be915310226bec9833
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-14 10:39:52 +0600

    Project13: Designing the interface

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var intensitySlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func changeFilter(_ sender: Any) {
+        
+    }
+    @IBAction func save(_ sender: Any) {
+        
+    }
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
 }
 
