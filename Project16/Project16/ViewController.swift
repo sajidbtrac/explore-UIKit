77b5d3b08be35e72cc04a758d3e197e72b52bf73
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-15 14:19:09 +0600

    Project 16: challenge 1 and 2

--- Project16/Project16/ViewController.swift
+++ Project16/Project16/ViewController.swift
@@ -20,20 +20,21 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
         let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
         
-//        mapView.addAnnotation(london)
-//        mapView.addAnnotation(oslo)
-//        mapView.addAnnotation(paris)
-//        mapView.addAnnotation(rome)
-//        mapView.addAnnotation(washington)
-        
+        //        mapView.addAnnotation(london)
+        //        mapView.addAnnotation(oslo)
+        //        mapView.addAnnotation(paris)
+        //        mapView.addAnnotation(rome)
+        //        mapView.addAnnotation(washington)
         mapView.addAnnotations([london, oslo, paris, rome, washington])
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(changeMapType))
     }
-
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is Capital else { return nil }
         
         let identifier = "Capital"
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
         
         if annotationView == nil {
             annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "Capital")
@@ -44,18 +45,43 @@ class ViewController: UIViewController, MKMapViewDelegate {
         } else {
             annotationView?.annotation = annotation
         }
+        //        annotationView?.tintColor = .green
+        annotationView?.markerTintColor = .green
         
         return annotationView
     }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         guard let capital = view.annotation as? Capital else { return }
-           let placeName = capital.title
-           let placeInfo = capital.info
-        
+        let placeName = capital.title
+        let placeInfo = capital.info
         
         let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func changeMapType() {
+        let ac = UIAlertController(title: "Pick MapType", message: nil, preferredStyle: .actionSheet)
+        let satellite = UIAlertAction(title: "Satellite", style: .default){ _ in
+            self.mapView.mapType = .satellite
+        }
+        let standard = UIAlertAction(title: "Standard", style: .default){ _ in
+            self.mapView.mapType = .standard
+        }
+        let hybrid = UIAlertAction(title: "Hybrid", style: .default){ _ in
+            self.mapView.mapType = .hybrid
+        }
+        let mutatedStandard = UIAlertAction(title: "Mutated Standard", style: .default){ _ in
+            self.mapView.mapType = .mutedStandard
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        ac.addAction(satellite)
+        ac.addAction(standard)
+        ac.addAction(hybrid)
+        ac.addAction(mutatedStandard)
+        ac.addAction(cancel)
         present(ac, animated: true)
     }
 }
