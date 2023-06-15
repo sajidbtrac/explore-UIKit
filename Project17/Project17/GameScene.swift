5d5d7f15214aba719eaf66334c7553bf5a0d01e6
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-15 15:46:13 +0600

    Project 17: Setting up

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by BTSL.SAJID on 15/6/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
