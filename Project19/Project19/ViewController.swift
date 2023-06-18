bd1f40e835e0bd77131af6fc8cc99b688c5afb4d
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-18 13:05:15 +0600

    Project 19: Setting up

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by BTSL.SAJID on 18/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
