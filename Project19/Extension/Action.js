b99c76a448f3e8326435e5155788be5cdd90fcbd
sajidbtrac <sajid.ios.btrac@gmail.com>
2023-06-18 13:54:39 +0600

    Project 19: What do you want to get?

---
+++ Project19/Extension/Action.js
@@ -0,0 +1,17 @@
+var Action = function() {};
+
+Action.prototype = {
+    
+//first is called before your extension is run, and the other is called after.
+
+run: function(parameters) {
+
+},
+
+finalize: function(parameters) {
+
+}
+
+};
+
+var ExtensionPreprocessingJS = new Action
