5e8e87d04465a79a35b3abbead6ed15e9fd30bcd
sajid <sajidhasan673@gmail.com>
2023-07-04 10:43:57 +0600

    project 23: Setting up

---
+++ Project23/Project23/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project23
+//
+//  Created by Sajid Shanta on 4/7/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+    
+    }
+}
